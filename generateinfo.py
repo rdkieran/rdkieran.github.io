18992e21bef7a99317788554085492c077ca3199
Robbie <73069287+rdkieran@users.noreply.github.com>
2023-05-29 19:08:49 +0100

    deleted images and changed website structure

--- generateinfo.py
+++
@@ -1,5 +0,0 @@
-for i in range (49):
-    filename = "img\\comics\\space_story_2\\pg{}.png".format(i+1)
-    href = "<img src=\"{}\">".format(filename)
-
-    print(href)
--- renamer.py
+++ renamer.py
@@ -2,23 +2,17 @@ import os
 
 def rename_files(folder_path):
     files = os.listdir(folder_path)
-    files.sort()  # Sort files alphabetically
-
+    files.sort()
 
     for i, file_name in enumerate(files):
-        # Construct new file name
         file_extension = os.path.splitext(file_name)[1]
-        new_file_name = f"pg{i+1}{file_extension}"  # e.g., 001.jpg, 002.jpg, ...
+        new_file_name = f"pg{i+1}{file_extension}"
 
-        # Rename the file
         old_file_path = os.path.join(folder_path, file_name)
         new_file_path = os.path.join(folder_path, new_file_name)
         os.rename(old_file_path, new_file_path)
 
         print(f"Renamed {file_name} to {new_file_name}")
 
-# Specify the folder path where the files are located
-folder_path = "img\\comics\\rat_boy_diaries\\hourlies_23"
-
-# Call the function to rename the files in the specified folder
+folder_path = "img\\comics\\space_story"
 rename_files(folder_path)
