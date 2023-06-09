b299459c30878a3df7935f28790f605f7b585108
Robbie <73069287+rdkieran@users.noreply.github.com>
2023-06-09 14:37:26 +0100

    rarebit logo moved to on .js file

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -2,16 +2,13 @@
 //at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeFooter"
 document.addEventListener('DOMContentLoaded', function() {
 document.querySelector(".writeFooter").innerHTML = `
-    <footer align="center">
-        <nav class="socialLinks">
-        <a href=""><img src="" alt="Twitter"></img></a>
-        <a href=""><img src="" alt="Instagram"></img></a>
-        <a href=""><img src="" alt="Ko-Fi"></img></a>
-        <a href=""><img src="" alt="Patreon"></img></a>
-        <a href=""><img src="" alt="LinkedIn"></img></a>
-        <a href="mailto:rdkieran.illu@gmail.com"><img src="" alt="email"></img></a>
-        <p><strong>Powered by</strong></p>
-        <a href="https://rarebit.neocities.org"><img src="img/rarebitlogo_small.png" height = "30" /></a>
-    </footer>
-`;
+    <nav class="socialLinks">
+        <a href="https://twitter.com/rdkieran" target="_blank"><img src="/img/footer/twitter.png" alt="Twitter"></img></a>
+        <a href="https://github.com/rdkieran" target="_blank"><img src="/img/footer/github.png" alt="GitHub"></img></a>
+        <a href="https://www.instagram.com/rdkieran/" target="_blank"><img src="/img/footer/instagram.png" alt="Instagram"></img></a>
+        <a href="https://www.linkedin.com/in/rdkieran/" target="_blank"><img src="/img/footer/linkedin.png" alt="LinkedIn"></img></a>
+        <a href="mailto:rdkieran.illu@gmail.com" target="_blank"><img src="/img/footer/email.png" alt="email"></img></a>
+    </nav>
+    <p class="copyright">© 2023 Robbie Kieran</p>
+    `
 })
