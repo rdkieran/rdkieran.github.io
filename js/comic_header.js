20bc1bfb81c0c7d5f10de08c8f0cec9c9362583b
Robbie <73069287+rdkieran@users.noreply.github.com>
2023-06-14 20:58:36 +0100

    removed banner 10 from banner list

--- js/comic_header.js
+++ js/comic_header.js
@@ -1,7 +1,7 @@
 //the header of the site would be handled in this javascript file, so you don't have to copypaste the whole thing onto every page.
 //at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeHeader"
 
-const extraBannerImages = ["/img/banner/robbie-1.png","/img/banner/robbie-2.png","/img/banner/robbie-3.png","/img/banner/robbie-4.png","/img/banner/robbie-5.png","/img/banner/robbie-6.png","/img/banner/robbie-7.png","/img/banner/robbie-8.png","/img/banner/robbie-9.png","/img/banner/robbie-10.png",];
+const extraBannerImages = ["/img/banner/robbie-1.png","/img/banner/robbie-2.png","/img/banner/robbie-3.png","/img/banner/robbie-4.png","/img/banner/robbie-5.png","/img/banner/robbie-6.png","/img/banner/robbie-7.png","/img/banner/robbie-8.png","/img/banner/robbie-9.png"];
 
 if (Math.floor(Math.random() * 5) > 0) {
     var bannerImage = "/img/banner/default.png";
