044d2566695b5ccfb27151e2f72385f610da655b
Robbie <73069287+rdkieran@users.noreply.github.com>
2023-06-21 17:26:50 +0100

    removed linkedin from footer (who cares!!!!!!)

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -6,9 +6,25 @@ document.querySelector(".writeFooter").innerHTML = `
         <a href="https://twitter.com/rdkieran" target="_blank"><img src="/img/footer/twitter.png" alt="Twitter"></img></a>
         <a href="https://github.com/rdkieran" target="_blank"><img src="/img/footer/github.png" alt="GitHub"></img></a>
         <a href="https://www.instagram.com/rdkieran/" target="_blank"><img src="/img/footer/instagram.png" alt="Instagram"></img></a>
-        <a href="https://www.linkedin.com/in/rdkieran/" target="_blank"><img src="/img/footer/linkedin.png" alt="LinkedIn"></img></a>
         <a href="mailto:rdkieran.illu@gmail.com" target="_blank"><img src="/img/footer/email.png" alt="email"></img></a>
     </nav>
     <p class="copyright">© 2023 Robbie Kieran</p>
     `
 })
+
+// ALL LINKS W/ HTML
+
+// TWITTER
+// <a href="https://twitter.com/rdkieran" target="_blank"><img src="/img/footer/twitter.png" alt="Twitter"></img></a>
+// BLUESKY
+// <a href="https://bsky.app/profile/rdkieran.bsky.social" target="_blank"><img src="" alt="Bluesky"></img></a>
+// INSTAGRAM
+// <a href="https://www.instagram.com/rdkieran/" target="_blank"><img src="/img/footer/instagram.png" alt="Instagram"></img></a>
+
+// GITHUB
+// <a href="https://github.com/rdkieran" target="_blank"><img src="/img/footer/github.png" alt="GitHub"></img></a>
+
+// LINKEDIN
+// <a href="https://www.linkedin.com/in/rdkieran/" target="_blank"><img src="/img/footer/linkedin.png" alt="LinkedIn"></img></a>
+// EMAIL
+// <a href="mailto:rdkieran.illu@gmail.com" target="_blank"><img src="/img/footer/email.png" alt="email"></img></a>
