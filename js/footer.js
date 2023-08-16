da67cdb3611bf2de6f538bdc05b3bc3d80542203
rdkieran <73069287+rdkieran@users.noreply.github.com>
2023-08-16 21:00:23 +0100

    v3: minimalist as fuck

--- js/footer.js
+++ js/footer.js
@@ -1,30 +1,9 @@
-//the footer of the site would be handled in this javascript file, so you don't have to copypaste the whole thing onto every page.
-//at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeFooter"
 document.addEventListener('DOMContentLoaded', function() {
-document.querySelector(".writeFooter").innerHTML = `
-    <nav class="socialLinks">
-        <a href="https://twitter.com/rdkieran" target="_blank"><img src="/img/footer/twitter.png" alt="Twitter"></img></a>
-        <a href="https://github.com/rdkieran" target="_blank"><img src="/img/footer/github.png" alt="GitHub"></img></a>
-        <a href="https://www.instagram.com/rdkieran/" target="_blank"><img src="/img/footer/instagram.png" alt="Instagram"></img></a>
-        <a href="mailto:rdkieran.illu@gmail.com" target="_blank"><img src="/img/footer/email.png" alt="email"></img></a>
-    </nav>
-    <p class="copyright">© 2023 Robbie Kieran</p>
-    `
-})
-
-// ALL LINKS W/ HTML
-
-// TWITTER
-// <a href="https://twitter.com/rdkieran" target="_blank"><img src="/img/footer/twitter.png" alt="Twitter"></img></a>
-// BLUESKY
-// <a href="https://bsky.app/profile/rdkieran.bsky.social" target="_blank"><img src="" alt="Bluesky"></img></a>
-// INSTAGRAM
-// <a href="https://www.instagram.com/rdkieran/" target="_blank"><img src="/img/footer/instagram.png" alt="Instagram"></img></a>
-
-// GITHUB
-// <a href="https://github.com/rdkieran" target="_blank"><img src="/img/footer/github.png" alt="GitHub"></img></a>
-
-// LINKEDIN
-// <a href="https://www.linkedin.com/in/rdkieran/" target="_blank"><img src="/img/footer/linkedin.png" alt="LinkedIn"></img></a>
-// EMAIL
-// <a href="mailto:rdkieran.illu@gmail.com" target="_blank"><img src="/img/footer/email.png" alt="email"></img></a>
+    document.querySelector("#writeFooter").innerHTML = `
+        <nav>
+            <li><a href="https://www.bsky.com/rdkieran">BlueSky</a></li>
+            <li><a href="https://www.twitter.com/rdkieran">X</a></li>
+            <li><a href="https://www.github.com/rdkieran">GitHub</a></li>
+        </nav>
+        <p class="copyright">© Robbie Kieran, 2023</p>
+`})
--- js/header.js
+++ js/header.js
@@ -1,27 +1,9 @@
-//the header of the site would be handled in this javascript file, so you don't have to copypaste the whole thing onto every page.
-//at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeHeader"
-
-//const extraBannerImages = ["/img/banner/default.png","/img/banner/robbie-1.png","/img/banner/robbie-2.png","/img/banner/robbie-3.png","/img/banner/robbie-4.png","/img/banner/robbie-5.png","/img/banner/robbie-6.png","/img/banner/robbie-7.png","/img/banner/robbie-8.png","/img/banner/robbie-9.png"];
-//if (Math.floor(Math.random() * 3) > 0) {
-//    var bannerImage = "/img/banner/default.png";
-//} else {
-//    var bannerImage = extraBannerImages[Math.floor(Math.random() * extraBannerImages.length)];
-//};
-
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector(".writeHeader").innerHTML = `
-    <nav>
-        <a href="/">home</a>
-        <a href="/about">about</a>
-        <a href="/wiki">wiki</a>
-    </nav>
-`;
-})
-
-//banner html if I want to add it back
-//<a href="/"><img src="${bannerImage}" alt="banner" class="bannerImage" /></a>
-
-/* old links to comics and blog indices, now found on main index
-        <a href="/comics.html">comics</a>
-        <a href="/blog.html">blog</a>
-*/
+    document.querySelector("#writeHeader").innerHTML = `
+        <h1><a href="/">Robbie's Website</a></h1>
+        <nav>
+            <li><a href="/">Home</a></li>
+            <li><a href="/comics/">Comics</a></li>
+            <li><a href="/blog/">Blog</a></li>
+        </nav>
+`})
