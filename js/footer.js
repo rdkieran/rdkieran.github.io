00cd9f5e71771a319509c1eefa3d8b93a94b0484
rdkieran <73069287+rdkieran@users.noreply.github.com>
2023-08-16 21:31:33 +0100

    highlight ongoing series

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#writeFooter").innerHTML = `
         <nav>
-            <li><a href="https://www.bsky.com/rdkieran">BlueSky</a></li>
+            <li><a href="https://www.bsky.app/profile/rdkieran.bsky.social">BlueSky</a></li>
             <li><a href="https://www.twitter.com/rdkieran">X</a></li>
             <li><a href="https://www.github.com/rdkieran">GitHub</a></li>
         </nav>
