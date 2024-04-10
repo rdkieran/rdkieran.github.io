8c688d6bd13e390718fb9ec961a575d03fbb3fe5
rdkieran <73069287+rdkieran@users.noreply.github.com>
2024-04-10 10:57:56 +0100

    centered header and footer? perhaps?

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#writeFooter").innerHTML = `
-        <nav>
-            <li><a href="https://www.bsky.app/profile/rdkieran.bsky.social">BlueSky</a></li>
-            <li><a href="https://www.twitter.com/rdkieran">X</a></li>
-            <li><a href="https://www.github.com/rdkieran">GitHub</a></li>
-        </nav>
-        <p class="copyright">© Robbie Kieran, 2023</p>
+        <center>
+            <nav>
+                <li><a href="https://www.bsky.app/profile/rdkieran.bsky.social">BlueSky</a></li>
+                <li><a href="https://www.twitter.com/rdkieran">X</a></li>
+                <li><a href="https://www.github.com/rdkieran">GitHub</a></li>
+            </nav>
+            <p class="copyright">© Robbie Kieran, 2023</p>
+        </center>
 `})
--- js/header.js
+++ js/header.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#writeHeader").innerHTML = `
-        <h1><a href="/">Robbie's Website</a></h1>
-        <nav>
-            <li><a href="/">Home</a></li>
-            <li><a href="/comics/">Comics</a></li>
-            <li><a href="/blog/">Blog</a></li>
-        </nav>
+        <center>
+            <h1><a href="/">Robbie's Website</a></h1>
+            <nav>
+                <li><a href="/">Home</a></li>
+                <li><a href="/comics/">Comics</a></li>
+                <li><a href="/blog/">Blog</a></li>
+            </nav>
+        </center>
 `})
