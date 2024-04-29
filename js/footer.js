bba4648614687478013a5d7f7c4498d7c010d07b
Robbie <73069287+rdkieran@users.noreply.github.com>
2024-04-29 16:24:48 +0100

    update footer links

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#writeFooter").innerHTML = `
         <nav>
-            <li><a href="https://www.bsky.app/profile/rdkieran.bsky.social">BlueSky</a></li>
-            <li><a href="https://www.twitter.com/rdkieran">X</a></li>
+            <li><a href="https://twitter.com/rdkieran">X</a></li>
+            <li><a href="https://bsky.app/profile/rdkieran.bsky.social">BlueSky</a></li> | 
             <li><a href="https://www.github.com/rdkieran">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/rdkieran/">LinkedIn</a></li>
         </nav>
         <p class="copyright">© Robbie Kieran, 2023</p>
 `})
