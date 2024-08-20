5015c3fb4f8b81ea44c8597beab93783e7d944b0
Robbie <73069287+rdkieran@users.noreply.github.com>
2024-08-20 23:35:57 +0100

    No more fucking around. I don't need to make this thing from scratch. I've backed up the old website locally and I'm going to ue Hugo instead. I'll put the comics back later.

--- js/header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector("#writeHeader").innerHTML = `
-        <h1><a href="/">Robbie's Website</a></h1>
-        <nav>
-            <li><a href="/">Home</a></li>
-            <li><a href="/comics/">Comics</a></li>
-            <li><a href="/blog/">Blog</a></li>
-        </nav>
-`})
--- js/rarebit_credit.js
+++
@@ -1,8 +0,0 @@
-//the footer of the site would be handled in this javascript file, so you don't have to copypaste the whole thing onto every page.
-//at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeFooter"
-document.addEventListener('DOMContentLoaded', function() {
-document.querySelector(".rarebitCredit").innerHTML = `
-    <p><strong>Powered by</strong></p>
-    <a href="https://rarebit.neocities.org" target="_blank"><img src="/img/footer/rarebitlogo_small.png" height = "30" /></a>
-    `
-})
