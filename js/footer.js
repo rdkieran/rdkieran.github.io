5b2ac94d63dc8edcb742c28173ac672274d88b2e
Robbie <73069287+rdkieran@users.noreply.github.com>
2024-10-11 15:27:28 +0100

    new pages

--- js/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector("#writeFooter").innerHTML = `
-        <nav>
-            <li><a href="https://twitter.com/rdkieran">X</a></li>
-            <li><a href="https://bsky.app/profile/rdkieran.bsky.social">BlueSky</a></li>
-            <li><a href="https://www.github.com/rdkieran">GitHub</a></li>
-        </nav>
-        <p class="copyright">© Robbie Kieran, 2023</p>
-`})
