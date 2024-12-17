b28406213596c0b54413654eb512eda52a5acf0b
Robbie <73069287+rdkieran@users.noreply.github.com>
2024-12-17 00:40:49 +0000

    target="_blank" for footer socials links

--- js/footer.js
+++ js/footer.js
@@ -3,12 +3,12 @@
 document.querySelector(".writeFooter").innerHTML = `
     <footer align="center">
         <div class="socialsNav">
-            <a href="https://www.patreon.com/rdkieran"><img src="/img/socials/patreon.png" alt="Patreon"></img></a>
-            <a href="https://ko-fi.com/rdkieran"><img src="/img/socials/kofi.png" alt="Ko-Fi"></img></a>
-            <a href="https://bsky.app/profile/rdkieran.bsky.social"><img src="/img/socials/bluesky.png" alt="BlueSky"></img></a>
-            <a href="https://www.instagram.com/rdkieran/"><img src="/img/socials/instagram.png" alt="Instagram"></img></a>
-            <a href="https://github.com/rdkieran"><img src="/img/socials/github.png" alt="GitHub"></img></a>
-            <a href="https://www.linkedin.com/in/rdkieran/"><img src="/img/socials/linkedin.png" alt="LinkedIn"></img></a>
+            <a target="_blank" href="https://www.patreon.com/rdkieran"><img src="/img/socials/patreon.png" alt="Patreon"></img></a>
+            <a target="_blank" href="https://ko-fi.com/rdkieran"><img src="/img/socials/kofi.png" alt="Ko-Fi"></img></a>
+            <a target="_blank" href="https://bsky.app/profile/rdkieran.bsky.social"><img src="/img/socials/bluesky.png" alt="BlueSky"></img></a>
+            <a target="_blank" href="https://www.instagram.com/rdkieran/"><img src="/img/socials/instagram.png" alt="Instagram"></img></a>
+            <a target="_blank" href="https://github.com/rdkieran"><img src="/img/socials/github.png" alt="GitHub"></img></a>
+            <a target="_blank" href="https://www.linkedin.com/in/rdkieran/"><img src="/img/socials/linkedin.png" alt="LinkedIn"></img></a>
         </div>
         <p>&copy; 2024 Robbie Kieran</p>
     </footer>
